98b3ca09b0611bcb175aad056de710c5c86ae3ab
Vladimir Romanov <valgard5@mail.ru>
2023-09-20 22:45:15 +0300

    Enemy behavior added

---
+++ Source/LeaveMeAlone/Enemy/LMAAIController.cpp
@@ -0,0 +1,17 @@
+// LeaveMeAlone Game by Netologiya. All Rights Reserved.
+
+
+#include "LMAAIController.h"
+#include "LeaveMeAlone/Enemy/LMAEnemyCharacter.h"
+
+void ALMAAIController::OnPossess(APawn *InPawn) 
+{
+  Super::OnPossess(InPawn);
+  const auto AICharacter = Cast<ALMAEnemyCharacter>(InPawn);
+  if (AICharacter) 
+  {
+    RunBehaviorTree(AICharacter->BehaviorTreeAsset);
+
+  }
+
+}
